7ef9c7ef50ddf98d60a3d18a7d249316d110cfe6
Orlikov Roman <102379576+OrlikovRoman@users.noreply.github.com>
2022-03-26 14:53:53 +0300

    Added variables
    Now you can create custom variables.

--- snt.py
+++ snt.py
@@ -1,8 +1,10 @@
 import os
+import random
 
 from colorama import init
 from colorama import Fore, Back, Style
 from datetime import datetime
+from ast import literal_eval
 init()
 
 def get_time():
@@ -22,57 +24,6 @@ def success(text):
 def error(text):
     print(f'{Style.DIM}{Fore.LIGHTRED_EX}[{get_time()}]{Fore.RESET}{Style.RESET_ALL} {text}')
 
-def stringify(object, indent:str = ' '):
-    tokens = {
-        'class': {},
-        'object': {},
-        'item': {}
-    }
-    result = f'property indent: "{indent}"\n\n'
-    temp_indent = ''
-    for element, value in object.items():
-        result += element + ':\n'
-        for object, value in value.items():
-            result += indent + object + ':\n'
-            for item, value in value.items():
-                result += indent + indent + item + ': ' + setType(value) + '\n'
-            result += indent + indent + 'end\n'
-        result += indent + 'end\n'
-
-    return result
-
-def to_dict(snt):
-    snt = snt.split('\n')
-    tokens = {}
-    is_first = True
-    indent = ' '
-    temp = ['', '', '']
-    for index, line in enumerate(snt):
-        if not line.endswith(':') and line.startswith('property'):
-            line = line.split(' ', 2)
-            property = line[1].rstrip(':')
-            if property == 'indent':
-                indent = line[2].strip('"').strip("'")
-        elif line == temp[2] + 'end':
-            temp[2] = temp[2].split(indent, 1)[1]
-        elif line.endswith(':') and not line.startswith(indent):
-            temp[2] += indent
-            tokens[line.rstrip(':')] = {}
-            temp[0] = line.rstrip(':')
-        elif line.endswith(':') and line.startswith(temp[2]):
-            temp[1] = line.rstrip(':').lstrip(temp[2])
-            tokens[temp[0]][line.rstrip(':').lstrip(temp[2])] = {}
-            temp[2] += indent
-        elif line and not line.isspace():
-            try:
-                line = line.lstrip(temp[2])
-                line = line.split(':', 1)
-                tokens[temp[0]][temp[1]][line[0]] = normalizeType(line[1].strip())
-            except IndexError as e:
-                error('SyntaxError: Invalid component or indent specified')
-                return [{}, ' ']
-    return [tokens, indent]
-
 def normalizeType(text):
     if text.startswith('"') and text.endswith('"') or text.startswith("'") and text.endswith("'"):
         if text.startswith('"') and text.endswith('"'):
@@ -81,19 +32,20 @@ def normalizeType(text):
             return text.strip("'")
     else:
         try:
-            text = int(text)
-            return text
+            return int(text)
         except:
-            pass
+            return f'%{text}%'
 
 def setType(text):
-    if isinstance(text, str):
+    if text.startswith('%') and text.endswith('%'):
+        return text.strip('%')
+    elif isinstance(text, str):
         return '"' + text + '"'
     else:
         return str(text)
 
 class SNT:
-    def __init__(self, path:str = '', indent:str = ' '):
+    def __init__(self, path:str = '', indent:str = 'none'):
         self.__indent = indent
         self.__file_found = True
         if path.endswith('/') or path == '':
@@ -113,18 +65,21 @@ class SNT:
 
         if not os.path.exists(self.__path):
             self.__file_found = False
-
-        data = self.__read()
-        if self.__file_found:
-            self.__data = data[0]
-            self.__indent = data[1]
-        else:
-            self.__data = {}
         
         if self.__file_found:
             success(f'Database «{self.name}» connected')
         else:
             error(f'File «{self.file}» not found')
+        
+        data = self.__read()
+        if self.__file_found:
+            self.__data = data[0]
+            if indent == 'none':
+                self.__indent = data[1]
+            else:
+                self.__indent = indent
+        else:
+            self.__data = {}
 
     @property
     def path(self):
@@ -134,9 +89,20 @@ class SNT:
     def print_path(self):
         log(self.__path)
     
+    def __get_data(self, data):
+        data = str(data)
+        for variable, value in self.__data.items():
+            if variable.startswith('%variable') and variable.endswith('%'):
+                data = data.replace(f'%{value[0]}%', value[1])
+        data = literal_eval(data)
+        for element in self.__data:
+            if element.startswith('%variable') and element.endswith('%'):
+                data.pop(element)
+        return data
+
     @property
     def data(self):
-        return self.__data
+        return self.__get_data(self.__data)
 
     def help(self):
         log('create')
@@ -145,7 +111,7 @@ class SNT:
         if self.__file_found:
             with open(self.__path, 'r') as file:
                 text = file.read()
-                return to_dict(text)
+                return self.to_dict(text)
 
     def set(self, object):
         if self.__file_found:
@@ -207,5 +173,62 @@ class SNT:
     def save(self):
         if self.__file_found:
             with open(self.__path, 'w') as file:
-                file.write(stringify(self.__data, self.__indent))
+                file.write(self.stringify(self.__data, self.__indent))
             log('SNT saved')
+
+    def stringify(self, object, indent:str = ' '):
+        tokens = {
+            'class': {},
+            'object': {},
+            'item': {}
+        }
+        result = f'property indent: "{indent}"\n\n'
+        for element, value in object.items():
+            if element.startswith('%variable') and element.endswith('%'):
+                result += f'variable {value[0]}: {setType(value[1])}\n'
+            else:
+                result += element + ':\n'
+                if not element.startswith('%variable') and not element.endswith('%'):
+                    for object, value in value.items():
+                        result += indent + object + ':\n'
+                        for item, value in value.items():
+                            result += indent + indent + item + ': ' + setType(value) + '\n'
+                        result += indent + indent + 'end\n'
+                result += indent + 'end\n'
+
+        return result
+
+    def to_dict(self, snt):
+        snt = snt.split('\n')
+        tokens = {}
+        is_first = True
+        indent = ' '
+        temp = ['', '', '']
+        for index, line in enumerate(snt):
+            if not line.endswith(':') and line.startswith('property'):
+                line = line.split(' ', 2)
+                property = line[1].rstrip(':')
+                if property == 'indent':
+                    indent = line[2].strip('"').strip("'")
+            elif not line.endswith(':') and line.startswith('variable'):
+                line = line.split(' ', 2)
+                tokens[f'%variable{random.randint(1, 1000000)}%'] = [line[1].rstrip(':'), normalizeType(line[2])]
+            elif line == temp[2] + 'end':
+                temp[2] = temp[2].split(indent, 1)[1]
+            elif line.endswith(':') and not line.startswith(indent):
+                temp[2] += indent
+                tokens[line.rstrip(':')] = {}
+                temp[0] = line.rstrip(':')
+            elif line.endswith(':') and line.startswith(temp[2]):
+                temp[1] = line.rstrip(':').lstrip(temp[2])
+                tokens[temp[0]][line.rstrip(':').lstrip(temp[2])] = {}
+                temp[2] += indent
+            elif line and not line.isspace():
+                try:
+                    line = line.lstrip(temp[2])
+                    line = line.split(':', 1)
+                    tokens[temp[0]][temp[1]][line[0]] = normalizeType(line[1].strip())
+                except IndexError as e:
+                    error('SyntaxError: Invalid component or indent specified')
+                    return [{}, ' ', {}]
+        return [tokens, indent]
